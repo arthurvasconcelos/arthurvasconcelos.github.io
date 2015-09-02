5c1080132fc87165dc95780a48e1771375875736
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2015-09-02 19:48:21 -0300

    Scripts fot hotsite

--- builder_files/js/main.js
+++ builder_files/js/main.js
@@ -1,2 +1,44 @@
 // Init ScrollReveal
-window.sr = new scrollReveal();
+var config = {
+  // easing: 'hustle',
+  reset:  true,
+  delay:  'onload',
+  vFactor: 0.90
+}
+window.sr = new scrollReveal(config);
+
+var DOMdocument = $(document);
+var DOMwindow = $(window);
+DOMdocument.ready(function(){
+  // Codes n' functions for Hotsite LandingPage
+  var hotLandingPage = $('#hotLandingPage');
+  if (hotLandingPage.length != 0) {
+    // Max height control
+    hotLandingPage.css('height', DOMdocument.height());
+    window.onresize = function(event) {
+      hotLandingPage.css('height', DOMdocument.height());
+    }
+
+    // Content vertical centralize
+    var hlpSegContainer = $('.js-vert-centralize');
+    hlpSegContainer.css({
+      'margin-top': hlpSegContainer.height() / 2 * (-1),
+      'margin-left': hlpSegContainer.width() / 2 * (-1)
+    });
+
+    // Socialbar centralize
+    var socialbar = $('.socialbar');
+    var socialbarWidth = 0;
+    var socialbarHeight = 0;
+
+    socialbar.find('li').each(function(){
+      socialbarWidth += $(this).outerWidth(true);
+      socialbarHeight = $(this).outerHeight(true);
+    });
+
+    socialbar.css({
+      width: socialbarWidth + parseInt(socialbar.css('padding')) * 2,
+      height: socialbarHeight + parseInt(socialbar.css('padding')) * 2
+    });
+  }
+});
