279cb7a4a330ee358f08d6bc1cca415852f1e2b8
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-09-29 00:40:52 -0300

    Adding logos

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import logo from '../static/images/logo-arthur-vasconcelos-horizontal.png';
 
 const Home = () =>
   <div>
     <h1>HOME</h1>
+    <img src={logo}/>
   </div>;
 
 
