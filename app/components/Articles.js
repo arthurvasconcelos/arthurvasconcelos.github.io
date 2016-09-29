aac021a3d2246b2acc8e5333bf6c963bdbc103ce
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-09-29 12:33:04 -0300

    Removing React

--- app/components/Articles.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Articles = () =>
-  <div>
-    <h1>ARTICLES</h1>
-  </div>;
-
-
-export default Articles;
--- app/components/Talks.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Talks = () =>
-  <div>
-    <h1>TALKS</h1>
-  </div>;
-
-
-export default Talks;
