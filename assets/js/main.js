1f3c937ff4f38ad050b1a231eda45b324fad42a7
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-10-03 11:37:07 -0300

    Implementing structures

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
-// teste
-const square = n => n * n;
+/**
+ * MAIN JS FILE
+ */
 
-let ola = "Que educado";
-// bobao
+// const mainHeader = document.querySelector('.main-header');
+// const ulNav = document.querySelector('.main-header nav ul');
+// const marginSpacer = (mainHeader.clientHeight - ulNav.clientHeight) / 2;
+// ulNav.style.margin = marginSpacer + 'px 0';
+
+// console.log(mainHeader);
+// console.log(ulNav);
+// console.log(marginSpacer);
+
+const links = document.querySelectorAll('.main-header nav li a');
+links.forEach((ev) => {
+  ev.addEventListener('click', (e) => {
+    // e.preventDefault();
+    e.target.setAttribute('class', 'active');
+  });
+});
