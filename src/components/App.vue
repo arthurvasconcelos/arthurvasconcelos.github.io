85f29d7f40cce5509a3d42847eb348da41e1d36f
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2018-07-23 19:01:09 -0300

    Initial setup with Parcel.JS

---
+++ src/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <h1>{{ msg }}</h1>
+        <div class="teste">testing <span class="bold">SCSS nest class</span></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data () {
+            return {
+                msg: 'Hey Therea'
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .teste {
+        color: #323234;
+
+        .bold {
+            font-weight: bold;
+            color: #f00;
+        }
+    }
+</style>
