cdcfeaefdee86bfb2de545be20f73e234b72d4e6
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2018-07-28 02:35:25 -0300

    Added font-awesome component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import router from './routes';
 
 import App from './components/app.vue';
 
 Vue.use(VueRouter);
+
+library.add([faCoffee, faBars, faTimes]);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
     el: '#app',
