fac89a3d89d88174101389c823436f4156e3f500
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2018-07-30 19:19:15 -0300

    Page content component

---
+++ src/components/shared/page-content.vue
@@ -0,0 +1,29 @@
+<template>
+    <article class="pageContent">
+        <slot></slot>
+    </article>
+</template>
+
+<script>
+    export default {
+        name: 'page-content',
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import '../../styles/variables';
+    @import '../../styles/mixins';
+
+    .pageContent {
+        @include applyFont(aux);
+
+        font-size: 1.5rem;
+        margin: 0 auto;
+        max-width: 1200px;
+        padding: .5em .8em;
+        width: 100%;
+    }
+</style>
