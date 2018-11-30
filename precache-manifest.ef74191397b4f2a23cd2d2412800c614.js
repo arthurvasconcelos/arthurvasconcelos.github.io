4ecb3c3af3a4e2b3b9d6e92c2a743e808917cdd4
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2018-11-30 16:18:40 -0200

    :rocket: deploy website

---
+++ precache-manifest.ef74191397b4f2a23cd2d2412800c614.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "e65330452221244bb3f6e26283397fd8",
+    "url": "/img/igarape_pt_block.e6533045.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "45af4fc7b5f022c0105e8bc7dacbc1db",
+    "url": "/img/profile.45af4fc7.jpg"
+  },
+  {
+    "revision": "8a9e415d05be2e6a4011",
+    "url": "/css/about-me~projects.40bf96fc.css"
+  },
+  {
+    "revision": "1f3642a8ddd20a7c761d",
+    "url": "/css/app.7f4ec02c.css"
+  },
+  {
+    "revision": "1f3642a8ddd20a7c761d",
+    "url": "/js/app.5b248344.js"
+  },
+  {
+    "revision": "c66aaa9e1c6667492e9f",
+    "url": "/js/chunk-vendors.83d493a4.js"
+  },
+  {
+    "revision": "f98218c1c5de5a9ffa88",
+    "url": "/css/projects.a853d097.css"
+  },
+  {
+    "revision": "f98218c1c5de5a9ffa88",
+    "url": "/js/projects.4aec00d8.js"
+  },
+  {
+    "revision": "25b18947bcd7f312b317194c113d956e",
+    "url": "/img/piloti.25b18947.png"
+  },
+  {
+    "revision": "ae29ac13a3df0bb9a10f652db4435de4",
+    "url": "/img/igarape_pt_wide.ae29ac13.png"
+  },
+  {
+    "revision": "c7fc2a0674e93d8ded7f",
+    "url": "/js/about-me.38880c88.js"
+  },
+  {
+    "revision": "8a9e415d05be2e6a4011",
+    "url": "/js/about-me~projects.cf8dd284.js"
+  },
+  {
+    "revision": "ac4141fcf91b814e142be7031afaba27",
+    "url": "/img/fgv-dapp.ac4141fc.png"
+  },
+  {
+    "revision": "c385365d815f08625791c58c00a71014",
+    "url": "/img/igarape_en_wide.c385365d.svg"
+  },
+  {
+    "revision": "c2a1cd2fcd3d5cfbe2b5f03719a8efec",
+    "url": "/img/igarape_en_block.c2a1cd2f.png"
+  },
+  {
+    "revision": "35b5158bb083eca3cf290e297e640d4d",
+    "url": "/img/igarape_pt_wide.35b5158b.svg"
+  },
+  {
+    "revision": "5fdb487fac821487ac4e99be79fb230d",
+    "url": "/img/igarape_en_block.5fdb487f.svg"
+  },
+  {
+    "revision": "8122f3ef8b8b73d69c38e77bc53a40e3",
+    "url": "/img/igarape_pt_block.8122f3ef.svg"
+  },
+  {
+    "revision": "b1f78968fbf28f11f8d49d8a2ff3fc3d",
+    "url": "/img/igarape_en_wide.b1f78968.png"
+  },
+  {
+    "revision": "71d4790adaa01fdf05aa9c194d8bdda8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7fc2a0674e93d8ded7f",
+    "url": "/css/about-me.5779a9dd.css"
+  }
+];
