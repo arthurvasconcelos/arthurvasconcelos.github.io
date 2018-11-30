59f8131351755600df31d0fbaca6371a95f52805
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2018-11-30 18:39:59 -0200

    :rocket: deploy website

---
+++ dist/README.md
@@ -0,0 +1,10 @@
+# arthurvasconcelos.github.io
+:metal: My personal website http://arthurvasconcelos.com.br
+
+|Branch|Tag|Description
+|------|---|---------
+|**master**|--|Production Version
+|**develop**|--|Active development version
+|**landing**|[v0.0.0](https://github.com/arthurvasconcelos/arthurvasconcelos.github.io/releases/tag/0.0.0)|Frist simple landing page
+|**v1**|[v1.0.0](https://github.com/arthurvasconcelos/arthurvasconcelos.github.io/releases/tag/1.0.0)|First try of redoing it
+|**v2**|[v2.0.0](https://github.com/arthurvasconcelos/arthurvasconcelos.github.io/releases/tag/2.0.0)|Tried Jekyll
---
+++ precache-manifest.032ea97dd9ff078faef177469efe7d00.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "e65330452221244bb3f6e26283397fd8",
+    "url": "/img/igarape_pt_block.e6533045.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "45af4fc7b5f022c0105e8bc7dacbc1db",
+    "url": "/img/profile.45af4fc7.jpg"
+  },
+  {
+    "revision": "525616cf92f0831c1fb4",
+    "url": "/css/about-me~projects.40bf96fc.css"
+  },
+  {
+    "revision": "bec3c5328e6d37a931c9",
+    "url": "/css/app.165871c4.css"
+  },
+  {
+    "revision": "bec3c5328e6d37a931c9",
+    "url": "/js/app.8d865bbf.js"
+  },
+  {
+    "revision": "c66aaa9e1c6667492e9f",
+    "url": "/js/chunk-vendors.83d493a4.js"
+  },
+  {
+    "revision": "f98218c1c5de5a9ffa88",
+    "url": "/css/projects.a853d097.css"
+  },
+  {
+    "revision": "f98218c1c5de5a9ffa88",
+    "url": "/js/projects.4aec00d8.js"
+  },
+  {
+    "revision": "25b18947bcd7f312b317194c113d956e",
+    "url": "/img/piloti.25b18947.png"
+  },
+  {
+    "revision": "ae29ac13a3df0bb9a10f652db4435de4",
+    "url": "/img/igarape_pt_wide.ae29ac13.png"
+  },
+  {
+    "revision": "d235a746aa6a632423ac",
+    "url": "/js/about-me.290ed357.js"
+  },
+  {
+    "revision": "525616cf92f0831c1fb4",
+    "url": "/js/about-me~projects.2cf9d1cf.js"
+  },
+  {
+    "revision": "ac4141fcf91b814e142be7031afaba27",
+    "url": "/img/fgv-dapp.ac4141fc.png"
+  },
+  {
+    "revision": "c385365d815f08625791c58c00a71014",
+    "url": "/img/igarape_en_wide.c385365d.svg"
+  },
+  {
+    "revision": "c2a1cd2fcd3d5cfbe2b5f03719a8efec",
+    "url": "/img/igarape_en_block.c2a1cd2f.png"
+  },
+  {
+    "revision": "35b5158bb083eca3cf290e297e640d4d",
+    "url": "/img/igarape_pt_wide.35b5158b.svg"
+  },
+  {
+    "revision": "5fdb487fac821487ac4e99be79fb230d",
+    "url": "/img/igarape_en_block.5fdb487f.svg"
+  },
+  {
+    "revision": "8122f3ef8b8b73d69c38e77bc53a40e3",
+    "url": "/img/igarape_pt_block.8122f3ef.svg"
+  },
+  {
+    "revision": "b1f78968fbf28f11f8d49d8a2ff3fc3d",
+    "url": "/img/igarape_en_wide.b1f78968.png"
+  },
+  {
+    "revision": "9368a7956b0bbf4aec47b6780458fe33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d235a746aa6a632423ac",
+    "url": "/css/about-me.cc489458.css"
+  }
+];
